759200cad9d1f0b35e8c84766ed9258da9671f44
suhyeon1103 <tngus74501@gmail.com>
2024-12-11 14:29:34 +0900

    Feat : 환경구축_1211

---
+++ TASKFLOW/frontend_2/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
