35bc82f7c66eab63da0a001888f59acc15909abb
FSO_01_하태웅_ <hamasu271@gmail.com>
2024-12-26 18:50:15 +0900

    로그인페이지 오류수정 및 쓸모없는 파일 삭제

--- TASKFLOW/frontend_2/src/App.tsx
+++ TASKFLOW/frontend_2/src/App.tsx
@@ -4,34 +4,32 @@ import LoginPage from "./pages/LoginPage.tsx";
 import SignupPage from "./pages/SignupPage.tsx";
 import SchedulePage from "./pages/SchedulePage.tsx";
 import HomePage from "./pages/HomePage.tsx";
-import Header from "./components/common/Header.tsx";
-import Footer from "./components/common/Footer.tsx";
-import Sidebar from "./components/common/Sidebar.tsx";
-import Project from "./pages/ProjectPage.tsx";
+import ProjectPage from "./pages/ProjectPage.tsx";
 import ProjectPageCreate from "./pages/ProjectPageCreate.tsx";
-
-import './App.css';
+import Header from "./components/common/Header.tsx";
+import Sidebar from "./components/common/Sidebar.tsx";
+import Footer from "./components/common/Footer.tsx";
 
 const App: React.FC = () => (
   <Router>
-    <div className="app-container">
-      <Sidebar />
-      <div className="main-container">
-        <Header />
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/schedule" element={<SchedulePage />} />
-            <Route path="/home" element={<HomePage />} /> 
-            <Route path="/project" element={<Project />} />
-            <Route path="/project/create" element={<ProjectPageCreate />} /> 
-          </Routes>
-        </main>
-      </div>
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/schedule" element={<Layout><SchedulePage /></Layout>} />
+      <Route path="/home" element={<Layout><HomePage /></Layout>} />
+      <Route path="/project" element={<Layout><ProjectPage /></Layout>} />
+      <Route path="/project/create" element={<Layout><ProjectPageCreate /></Layout>} />
+    </Routes>
   </Router>
+);
+
+const Layout: React.FC = ({ children }) => (
+  <div>
+    <Header />
+    <Sidebar />
+    <main>{children}</main>
+    <Footer />
+  </div>
 );
 
 export default App; 
